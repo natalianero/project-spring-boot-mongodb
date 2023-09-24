88719a4aa9566ec767dff6d29097fcd682a2fa78
natalianero <nataliasilva10@gmail.com>
2023-09-24 17:09:15 -0300

    Insercao de usuario com POST

--- src/main/java/com/natalianero/projectmongo/services/UserService.java
+++ src/main/java/com/natalianero/projectmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.natalianero.projectmongo.domain.User;
+import com.natalianero.projectmongo.dto.UserDTO;
 import com.natalianero.projectmongo.repository.UserRepository;
 import com.natalianero.projectmongo.services.exception.ObjectNotFoundException;
 
@@ -16,13 +17,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
