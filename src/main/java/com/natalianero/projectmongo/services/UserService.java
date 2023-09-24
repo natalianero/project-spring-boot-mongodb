e96739dd264e259ec45d254b8c27854c276ddc02
natalianero <nataliasilva10@gmail.com>
2023-09-24 16:21:16 -0300

    Obtendo um usuario por id

--- src/main/java/com/natalianero/projectmongo/services/UserService.java
+++ src/main/java/com/natalianero/projectmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.natalianero.projectmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.natalianero.projectmongo.domain.User;
 import com.natalianero.projectmongo.repository.UserRepository;
+import com.natalianero.projectmongo.services.exception.ObjectNotFoundException;
 
 
 @Service
@@ -17,5 +19,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
