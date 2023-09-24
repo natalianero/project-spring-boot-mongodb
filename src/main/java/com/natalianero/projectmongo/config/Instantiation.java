f18b87cab9abc23f14abd1c6e34f19b076b3a6bd
natalianero <nataliasilva10@gmail.com>
2023-09-24 18:37:04 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/natalianero/projectmongo/config/Instantiation.java
+++ src/main/java/com/natalianero/projectmongo/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.natalianero.projectmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.natalianero.projectmongo.domain.Post;
 import com.natalianero.projectmongo.domain.User;
+import com.natalianero.projectmongo.dto.AuthorDTO;
+import com.natalianero.projectmongo.repository.PostRepository;
 import com.natalianero.projectmongo.repository.UserRepository;
 
 
@@ -15,17 +20,29 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userReposiroty;
-	
+
+	@Autowired
+	private PostRepository postReposiroty;
+
 	@Override
 	public void run(String... arg0) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
+		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/natalianero/projectmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.natalianero.projectmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.natalianero.projectmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
