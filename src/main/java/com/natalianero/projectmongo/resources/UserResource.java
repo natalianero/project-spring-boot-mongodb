d6b71c20ed11bd85ebb9f9337a342f4f6db54c33
natalianero <nataliasilva10@gmail.com>
2023-09-24 19:03:33 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/natalianero/projectmongo/resources/UserResource.java
+++ src/main/java/com/natalianero/projectmongo/resources/UserResource.java
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.natalianero.projectmongo.domain.Post;
 import com.natalianero.projectmongo.domain.User;
 import com.natalianero.projectmongo.dto.UserDTO;
 import com.natalianero.projectmongo.services.UserService;
-
 
 
 @RestController
@@ -59,5 +59,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
