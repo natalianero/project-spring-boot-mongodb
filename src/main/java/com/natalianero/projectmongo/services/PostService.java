ceb14de57b3ead73c311df71b8c76f70a4ee5b2a
natalianero <nataliasilva10@gmail.com>
2023-09-24 19:14:06 -0300

    Obtendo um post por id

---
+++ src/main/java/com/natalianero/projectmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.natalianero.projectmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natalianero.projectmongo.domain.Post;
+import com.natalianero.projectmongo.repository.PostRepository;
+import com.natalianero.projectmongo.services.exception.ObjectNotFoundException;
+
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
