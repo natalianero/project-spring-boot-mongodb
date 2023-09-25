ea4e2d60604dd24c1125ca2555087f64f7f8e7e3
natalianero <nataliasilva10@gmail.com>
2023-09-25 20:30:22 -0300

    Consulta simples com query methods

--- src/main/java/com/natalianero/projectmongo/resources/PostResource.java
+++ src/main/java/com/natalianero/projectmongo/resources/PostResource.java
@@ -1,14 +1,19 @@
 package com.natalianero.projectmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.natalianero.projectmongo.domain.Post;
+import com.natalianero.projectmongo.resources.util.URL;
 import com.natalianero.projectmongo.services.PostService;
+
 
 @RestController
 @RequestMapping(value="/posts")
@@ -21,5 +26,12 @@ public class PostResource {
  	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
